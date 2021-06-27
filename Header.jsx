edcfb4a1e42e314b8995982c68f1a919b80a6027
shaloo-kumari <86568831+shaloo-kumari@users.noreply.github.com>
2021-06-27 23:16:40 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Overview of Java</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Object-Oriented Programming</h1>
+      <p>
+        Object-oriented programming (OOP) is at the core of Java. In fact, all
+        Java programs are to at least some extent object-oriented. OOP is so
+        integral to Java that it is best to understand its basic principles
+        before you begin writing even simple Java programs. Therefore, this
+        chapter begins with a discussion of the theoretical aspects of OOP.
+      </p>
+    </div>
+  );
+}
+export default Note;
